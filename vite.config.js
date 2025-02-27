959d7772320229067e45c74eb79985dc02e6ea6e
Senuka Chandunu <senukaacc@gmail.com>
2025-02-28 01:03:02 +0530

    Netlify dist settings update

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/./',
   plugins: [react()],
+  build: {
+    outDir: 'dist'},
   css: {
     preprocessorOptions: {
       scss: {
